4e915596318a079d61e29abf4641489718e473b3
John Selbie <jselbie@microsoft.com>
2021-04-11 11:04:56 -0700

    ttt game mostly working

--- src/App.js
+++ src/App.js
@@ -1,25 +1,213 @@
 import logo from './logo.svg';
 import './App.css';
+import BoardModel from './BoardModel.js';
+import React, {useEffect, useState} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const AreaSquare = (props) => {
+  var coordinates = props.left.toString() + "," + props.top.toString() + "," + props.right.toString() + "," + props.bottom.toString();
+  return <area coords={coordinates} shape="rect" onClick={props.callback} />
+};
+
+const CellImage = (props) => {
+  const leftOffset = ((props.cellIndex % 3) * 200).toString() + "px";
+  const topOffset = (Math.floor((props.cellIndex / 3)) * 200).toString() + "px";
+  const src = (props.cellValue === 1) ? "x.png" : "o.png";
+
+  if (props.cellValue === 0) {
+    return (<></>);
+  }
+
+  var opacity = (props.hover) ? .1 : 1;
+
+  return <img key={props.cellIndex} src={src} style={{position:"absolute", left:leftOffset, top:topOffset, pointerEvents:"none", opacity:opacity}} />
+};
+
+const Board = (props) => {
+
+  let boardState = props.boardState;
+  const cellIndices = [0,1,2,3,4,5,6,7,8];
+  const [hoverIndex, setHoverIndex] = useState(-1);
+  
+  var styles = {
+    margin:"0 auto",
+    position:"relative",
+    width:"600px"
+  };
+
+  const cellCallback = (cellNum) => {
+    props.cellCallback(cellNum);
+  };
+
+  const onMouseEnter = (event) => {
+    
+    // https://stackoverflow.com/questions/3234256/find-mouse-position-relative-to-element/42111623#42111623
+
+    console.log("onMouseEnter", event.clientX, event.clientY);
+    let boardDiv = document.getElementById("boarddiv");
+    let boardImg = document.getElementById("boardimg");
+
+    if (boardDiv && boardImg) {
+      var boardRect = boardDiv.getBoundingClientRect();
+      var imgRect = boardImg.getBoundingClientRect();
+
+      console.log("boardRect", Math.floor(boardRect.left), Math.floor(boardRect.top), Math.floor(boardRect.right), Math.floor(boardRect.bottom));
+      console.log("imgRect", Math.floor(imgRect.left), Math.floor(imgRect.top), Math.floor(imgRect.right), Math.floor(imgRect.bottom));
+
+      var x = event.clientX - imgRect.left;
+      var y = event.clientY - imgRect.top;
+      console.log("normalized position", x, y);
+
+      x = Math.floor(x / 200);
+      y = Math.floor(y / 200);
+
+      if ((x >= 3) || (y >= 3)) {
+        setHoverIndex(-1);
+      } else {
+        console.log("setting hover index to ", y*3+x);
+        setHoverIndex(y*x+3);
+      }
+
+    }
+
+
+
+    //setHoverIndex(cellNum);
+  };
+
+  const onMouseLeave = (cellNum) => {
+    console.log("onMouseLeave", cellNum);
+    //if (hoverIndex === cellNum) {
+    //  setHoverIndex(-1);
+    //}
+  };
+
+  const GenerateAreaSquare = (cellNum) => {
+    let left = (cellNum%3)*200;
+    let top = Math.floor(cellNum/3)*200;
+    let right = left + 200;
+    let bottom = top + 200
+    return <AreaSquare left={left} top={top} right={right} bottom={bottom} callback={()=>cellCallback(cellNum)} ></AreaSquare>
+  }
+
+
+  return(
+    <div id="boarddiv" style={styles} onMouseMove={onMouseEnter} onMouseOut={onMouseLeave}>
+      <img id="boardimg" src="board.png" useMap="#boardmap"  />
+
+      {cellIndices.map(index=>  <CellImage cellIndex={index} cellValue={boardState[index]} /> )}
+
+      {cellIndices.map(index=>  (index===hoverIndex)?<CellImage cellIndex={index} cellValue={props.playerIsX?1:2} hover={true} />:<></> )};
+
+      <map id="boardmap" name="boardmap" style={{position:"absolute"}} >
+        {cellIndices.map(cellNum => GenerateAreaSquare(cellNum))}
+      </map>
+
     </div>
   );
+};
+
+const Controls = (props) => {
+
+  const [playerIsX, setPlayerIsX] = useState(true);
+
+  const onChangeHandler = (event) => {
+    console.log("onChangeHandler: ", event.target.value);
+    var playerIsXUpdated = event.target.value==="first"
+    setPlayerIsX(playerIsXUpdated);
+  };
+
+  const onSubmit = () => {
+    console.log("onSubmit", playerIsX);
+    props.onNewGame(playerIsX);
+  }
+
+  return (
+  <center>
+    <input name="radgroup1" type="radio" value={"first"} checked={playerIsX}  onChange={onChangeHandler}/>Local Player is X &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+    <input name="radgroup1" type="radio" value={"second"} checked={!playerIsX} onChange={onChangeHandler}/>Computer is X 
+    <p></p>
+    <input type="button" value="Start New Game" onClick={onSubmit}  />
+    <p></p>
+    <h1>{props.message}</h1>
+  </center>
+  );
+
+};
+
+
+
+const DumpBoard = (cells) => {
+
+  var display = cells.map(val=> {
+    if (val === 0) return "_";
+    if (val === 1) return "X";
+    if (val === 2) return "O";
+    return "?"
+
+  });
+
+  console.log("\n\n============================")
+  console.log(display[0], display[1], display[2]);
+  console.log(display[3], display[4], display[5]);
+  console.log(display[6], display[7], display[8]);
+  console.log("============================")
 }
+
+const BoardDiv = (props) => {
+
+  let [boardModel, setBoardModel] = useState(null);
+
+  if (boardModel === null) {
+    boardModel = new BoardModel(true);
+    setBoardModel(boardModel);
+  }
+
+  var styles = {
+    backgroundColor:"Silver",
+    margin:"0 auto",
+    width:"800px",
+    position:"relative",
+    border:"thin solid black"
+  };
+
+  const cellCallback = (cellNum) => {
+    console.log("click on cell: ", cellNum);
+    boardModel.playerMakeMove(cellNum);
+    setBoardModel(boardModel.clone());
+  };
+  
+  const startNewGame = (playerIsX)  => {
+    setBoardModel(new BoardModel(playerIsX));
+  }
+
+  var message = "";
+  if (boardModel.winner === boardModel.TIE) {
+    message = "Tie Game!";
+  } else if (boardModel.winner === boardModel.X) {
+    message = "X is Winner!";
+  } else if (boardModel.winner === boardModel.O) {
+    message = "O is Winner!";
+  }
+
+  //DumpBoard(boardModel.boardState);
+  var playerIsX = (boardModel.localPlayerValue===boardModel.X);
+
+  return (
+  <div style={styles}>
+    <center><h1>Tic Tac Toe</h1></center>
+    <Board cellCallback={cellCallback} boardState={boardModel.boardState} playerIsX={playerIsX} />
+    <Controls onNewGame={startNewGame} message={message}/>
+  </div>
+  );
+
+
+};
+
+const App = () => {
+  console.log("RENDERING APP");
+  return (
+   <BoardDiv/>
+  );
+};
 
 export default App;
---
+++ src/BoardModel.js
@@ -0,0 +1,155 @@
+
+class BoardModel
+{
+    constructor(playerIsX) {
+
+        this.UNSPECIFIED = 0;
+        this.X = 1;
+        this.O = 2;
+        this.TIE = 3;
+
+        this.boardState = [0,0,0,  0,0,0,  0,0,0];
+        this.winner = this.UNSPECIFIED; // 0 is no winer, 1 is X, 2 is Y, 3 is TIE
+        this.localPlayerValue = (playerIsX ? this.X : this.Y);
+
+        if (!playerIsX) {
+            this._computerMakeMove();
+        }
+    }
+
+    getBoardState() {
+        return [...this.boardState];
+    }
+
+    clone() {
+        // https://stackoverflow.com/questions/41474986/how-to-clone-a-javascript-es6-class-instance
+        return Object.assign(Object.create(Object.getPrototypeOf(this)), this);
+    }
+
+    playerMakeMove(cellIndex) {
+
+        if (this.winner !== this.UNSPECIFIED) {
+            console.log("the game is over")
+            return;
+        }
+
+        if (this.boardState[cellIndex] !== this.UNSPECIFIED) {
+            console.assert(false, "cell is already taken");
+            return;
+        }
+
+        this._applyMove(cellIndex, this.localPlayerValue);
+        if (this.winner === this.UNSPECIFIED) {
+            this._computerMakeMove();
+        }
+    }
+
+    _computerMakeMove() {
+        
+        var madeMove = false;
+        var computerValue = (this.localPlayerValue === this.X) ? this.O : this.X;
+
+        let i = 0;
+
+        // look for a winning move
+        for (i = 0; i < 9; i++) {
+            let tmpState = [...this.boardState];
+            if (tmpState[i] === this.UNSPECIFIED) {
+                tmpState[i] = computerValue;
+                if (this._checkForWin(tmpState, computerValue)) {
+                    this._applyMove(i, computerValue);
+                    madeMove = true;
+                    break;
+                }
+            }
+        }
+
+        // look for a blocking mode
+        if (!madeMove) {
+            for (i = 0; i < 9; i++) {
+                let tmpState = [...this.boardState];
+                if (tmpState[i] === this.UNSPECIFIED) {
+                    tmpState[i] = this.localPlayerValue;
+                    if (this._checkForWin(tmpState, this.localPlayerValue)) {
+                        this._applyMove(i, computerValue);
+                        madeMove = true;
+                        break;
+                    }
+                }
+            }
+        }
+
+        // look for center
+        if (!madeMove) {
+            if (this.boardState[4] === this.UNSPECIFIED) {
+                this._applyMove(4, computerValue);
+                madeMove = true;
+            }
+        }
+
+        // pick a random cell
+        if (!madeMove) {
+            var available = [];
+            for (i = 0; i < 9; i++) {
+                if (this.boardState[i] === this.UNSPECIFIED) {
+                    available.push(i);
+                }
+            }
+            if (available.length > 0) {
+                var pick = available[Math.floor(Math.random() * available.length)];
+                this._applyMove(pick, computerValue);
+                madeMove = true;
+            }
+        }
+
+        return madeMove;
+    }
+
+    _applyMove(cellIndex, playerValue) {
+
+        console.assert(this.boardState[cellIndex] === this.UNSPECIFIED);
+
+        this.boardState[cellIndex] = playerValue;
+
+        if (this._checkForWin(this.boardState, playerValue)) {
+            this.winner = playerValue;
+        }
+
+        if (this._checkForTie()) {
+            this.winner = 3;
+        }
+
+    }
+
+    _checkForWin(cells, player) {
+
+        var wincombo0 = [0,1,2];
+        var wincombo1 = [3,4,5];
+        var wincombo2 = [6,7,8];
+        var wincombo3 = [0,3,6];
+        var wincombo4 = [1,4,7];
+        var wincombo5 = [2,5,8];
+        var wincombo6 = [0,4,8];
+        var wincombo7 = [2,4,6];
+        var combos = [wincombo0, wincombo1, wincombo2, wincombo3, wincombo4, wincombo5, wincombo6, wincombo7];
+
+        for (var i = 0; i < combos.length; i++) {
+            var wincombo = combos[i];
+            if (cells[wincombo[0]]===player && cells[wincombo[1]]===player && cells[wincombo[2]]===player) {
+                return true;
+            }
+        }
+        return false;
+    };
+
+    _checkForTie() {
+        for (var i = 0; i < 9; i++) {
+            if (this.boardState[i] === 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
+
+export default BoardModel;
